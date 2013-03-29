95c3c1fe0cb5b3ab27f4e040939ef5ecf27536ff
pepelazz <pepelazz00@gmail.com>
2013-03-29 19:13:40 +0400

    tabula display + include

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,25 +1,41 @@
 package controllers;
 
+import models.Article;
 import play.*;
 import play.db.jpa.JPA;
-import play.libs.Codec;
+import play.exceptions.TemplateNotFoundException;
 import play.mvc.*;
 
-import java.util.*;
-
-import models.*;
-
 import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<Article> articles = Article.findAll();
+        render(articles);
     }
 
-    public static void article(Long id) {
-        Article article = Article.findById(id);
-        render();
+    public static void article (String name) {
+        final TypedQuery<Article> q = JPA.em().createQuery(
+                "SELECT a FROM Article a WHERE a.name = :name",
+                Article.class);
+        q.setParameter("name", name);
+        Article article = q.getSingleResult();
+        render(article);
     }
+
+//    public static void article (String name) {
+//
+//        System.out.println("name = " + name);
+//
+//        Logger.debug("Requested: '%s.html'", name);
+//        try {
+//            render("articles/" + name + ".html");
+//        } catch (TemplateNotFoundException e) {
+//            Logger.error("Missing file: '%s.html'", name);
+//            notFound();
+//        }
+//    }
 
 }
